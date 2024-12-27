3e2c49d2378dc31c8812462b79cf7dd28248f1ac
Ivan Litov <ivanslitov@gmail.com>
2024-12-27 14:31:15 +0200

    Add clang format

--- VectorAddition/main.cpp
+++ VectorAddition/main.cpp
@@ -1,24 +1,25 @@
-#include "utils.hpp"
-
 #include <ranges>
 
+#include "cuda_runtime.h"
+#include "utils.hpp"
+
 void sequentialVecAdd(const std::vector<float> &veca, const std::vector<float> &vecb, std::vector<float> &vecc) {
-	for (auto [a, b, c] : std::views::zip(veca, vecb, vecc)) {
-		c = a + b;
-	}
+    for (auto [a, b, c] : std::views::zip(veca, vecb, vecc)) {
+        c = a + b;
+    }
 }
 
 int main() {
-	const std::size_t N = 100'000;
+    const std::size_t N = 100'000;
 
-	std::vector<float> a = utils::randVec<float>(N);
-	std::vector<float> b = utils::randVec<float>(N);
-	std::vector<float> c(a.size());
+    std::vector<float> a = utils::randVec<float>(N);
+    std::vector<float> b = utils::randVec<float>(N);
+    std::vector<float> c(a.size());
 
-	{
-		utils::Time <std::chrono::microseconds> _;
-		sequentialVecAdd(a, b, c);
-	}
+    {
+        utils::Time<std::chrono::microseconds> _{"Seq"};
+        sequentialVecAdd(a, b, c);
+    }
 
-	return 0;
+    return 0;
 }
--- utils/utils.hpp
+++ utils/utils.hpp
@@ -1,57 +1,60 @@
 #ifndef _UTILS_HPP_
 #define _UTILS_HPP_
 
-#include <iostream>
-#include <vector>
 #include <chrono>
-#include <type_traits>
+#include <iostream>
 #include <string_view>
+#include <type_traits>
+#include <vector>
 
 namespace utils {
-	template <typename Duration = std::chrono::milliseconds>
-	struct Time {
-		std::chrono::high_resolution_clock::time_point started;
-		Time() {
-			started = std::chrono::high_resolution_clock::now();
-		}
-		~Time() {
-			auto elapsedTime = std::chrono::high_resolution_clock::now() - started;
-			std::cout << "Elapsed " << std::chrono::duration_cast<Duration>(elapsedTime).count() << durationToStr() << '\n';
-		}
-		static std::string_view durationToStr() {
-			if constexpr (std::is_same_v<Duration, std::chrono::seconds>) {
-				return "s";
-			}
-			if constexpr (std::is_same_v<Duration, std::chrono::milliseconds>) {
-				return "ms";
-			}
-			if constexpr (std::is_same_v<Duration, std::chrono::microseconds>) {
-				return "us";
-			}
-			if constexpr (std::is_same_v<Duration, std::chrono::nanoseconds>) {
-				return "ns";
-			}
-		}
-	};
+template <typename Duration = std::chrono::milliseconds>
+struct Time {
+    std::chrono::high_resolution_clock::time_point started;
+    std::string_view text;
+    Time(std::string_view text = "")
+        : started(std::chrono::high_resolution_clock::now())
+        , text(text) {
+    }
+    ~Time() {
+        auto elapsedTime = std::chrono::high_resolution_clock::now() - started;
+        std::cout << text << "; Elapsed " << std::chrono::duration_cast<Duration>(elapsedTime).count() << durationToStr()
+                  << '\n';
+    }
+    static std::string_view durationToStr() {
+        if constexpr (std::is_same_v<Duration, std::chrono::seconds>) {
+            return "s";
+        }
+        if constexpr (std::is_same_v<Duration, std::chrono::milliseconds>) {
+            return "ms";
+        }
+        if constexpr (std::is_same_v<Duration, std::chrono::microseconds>) {
+            return "us";
+        }
+        if constexpr (std::is_same_v<Duration, std::chrono::nanoseconds>) {
+            return "ns";
+        }
+    }
+};
 
-	template <typename T>
-	inline void print(const std::vector<T> &vec) {
-		for (T v : vec) {
-			std::cout << v << ", ";
-		}
-		std::cout << '\n';
-	}
-
-	template <typename T>
-	inline std::vector<T> randVec(std::size_t n) {
-		std::vector<T> vec(n);
-
-		for (std::size_t i = 0; i < n; ++i) {
-			vec[i] = static_cast<T>(i);
-		}
-
-		return vec;
-	}
+template <typename T>
+inline void print(const std::vector<T> &vec) {
+    for (T v : vec) {
+        std::cout << v << ", ";
+    }
+    std::cout << '\n';
 }
 
-#endif // !_UTILS_HPP_
+template <typename T>
+inline std::vector<T> randVec(std::size_t n) {
+    std::vector<T> vec(n);
+
+    for (std::size_t i = 0; i < n; ++i) {
+        vec[i] = static_cast<T>(i);
+    }
+
+    return vec;
+}
+}  // namespace utils
+
+#endif  // !_UTILS_HPP_
